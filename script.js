6d7c6ef2920d06cd96a188e5ebdfbb756461d882
ivanshaoo41 <142716124+ivanshaoo41@users.noreply.github.com>
2025-12-21 19:08:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function openGift() {
 
 /* LOVE COUNTER */
 function startCounter() {
-  const startDate = new Date("2024-09-18T00:00:00");
+  const startDate = new Date("2025-09-18T00:00:00");
 
   setInterval(() => {
     const now = new Date();
